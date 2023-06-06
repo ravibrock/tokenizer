e2d6c2280b9cb70d408aaed424187f3bd8461e21
ravibrock <66334356+ravibrock@users.noreply.github.com>
2023-06-06 09:57:11 -0400

    Remove write option

--- tokenizer.py
+++ tokenizer.py
@@ -14,7 +14,6 @@ def connect(file):
 def main():
     parser = argparse.ArgumentParser("Count tokens in a file")
     parser.add_argument("input", help="Text file", type=str, default=None)
-    parser.add_argument("-w", "--write", help="Path to write tokenized file to", type=str, default=None, required=False)
     args = parser.parse_args()
     if args.input is None:
         raise Exception("First argument must be filepath to tokenize.")
@@ -24,10 +23,6 @@ def main():
     for sublist in tokens["input_ids"]:
         count += len(sublist)
     print(f"Number of tokens: {count}")
-
-    if args.write is not None:
-        with open(exp(args.write), "w") as f:
-            f.write(str(tokens))
 
 
 if __name__ == "__main__":
